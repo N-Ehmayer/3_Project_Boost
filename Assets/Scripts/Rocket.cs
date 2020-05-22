de639f9ed93a22dc4c16db5cbf3be3655d8f5db6
N-Ehmayer <nic.ehmayer92@gmail.com>
2020-05-21 22:58:11 -0600

    Sound

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    bool thrusting = false;
+    bool playingSound = false;
+
     // Start is called before the first frame update
     void Start() {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -14,14 +22,23 @@ public class Rocket : MonoBehaviour {
     }
 
     private void ProcessInput() {
+
         if (Input.GetKey(KeyCode.Space)) {
             print("Space pressed");
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+        } else {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A)) {
             print("Left pressed");
+            transform.Rotate(Vector3.forward);
         } else if (Input.GetKey(KeyCode.D)) {
             print("Right pressed");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
